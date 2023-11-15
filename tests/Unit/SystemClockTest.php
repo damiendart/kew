e6ae782aff19adb960439a3297aec0fd6a194de5
Damien Dart <damiendart@pobox.com>
2023-11-15 23:51:00 +0000

    Add a couple of "SystemClock" tests.

---
+++ tests/Unit/SystemClockTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * Copyright (C) Damien Dart, <damiendart@pobox.com>.
+ * This file is distributed under the MIT licence. For more information,
+ * please refer to the accompanying "LICENCE" file.
+ */
+
+declare(strict_types=1);
+
+namespace Kew\Tests\Unit;
+
+use Kew\SystemClock;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Kew\SystemClock
+ *
+ * @internal
+ */
+final class SystemClockTest extends TestCase
+{
+    public function test_returns_the_current_time(): void
+    {
+        $clock = new SystemClock();
+
+        $this->assertNotSame($clock->now(), $clock->now());
+    }
+
+    public function test_defaults_to_current_timezone_if_not_specified(): void
+    {
+        $clock = new SystemClock();
+        $now = $clock->now();
+
+        $this->assertSame(
+            date_default_timezone_get(),
+            $now->getTimezone()->getName(),
+        );
+    }
+}
