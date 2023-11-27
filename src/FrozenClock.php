9495046a2eb1739fb2c053951bfa6db630b885ca
Damien Dart <damiendart@pobox.com>
2023-11-27 22:58:26 +0000

    Add an initial implementation of a "frozen" clock.

---
+++ src/FrozenClock.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Copyright (C) Damien Dart, <damiendart@pobox.com>.
+ * This file is distributed under the MIT licence. For more information,
+ * please refer to the accompanying "LICENCE" file.
+ */
+
+declare(strict_types=1);
+
+namespace DamienDart\Kew;
+
+use Psr\Clock\ClockInterface;
+
+/** @psalm-api */
+class FrozenClock implements ClockInterface
+{
+    public function __construct(
+        private \DateTimeImmutable $frozenAt,
+    ) {}
+
+    public function now(): \DateTimeImmutable
+    {
+        return clone $this->frozenAt;
+    }
+
+    public function setTo(\DateTimeImmutable $dateTime): self
+    {
+        $this->frozenAt = $dateTime;
+
+        return $this;
+    }
+}
---
+++ tests/Unit/FrozenClockTest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * Copyright (C) Damien Dart, <damiendart@pobox.com>.
+ * This file is distributed under the MIT licence. For more information,
+ * please refer to the accompanying "LICENCE" file.
+ */
+
+declare(strict_types=1);
+
+namespace DamienDart\Kew\Tests\Unit;
+
+use DamienDart\Kew\FrozenClock;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \DamienDart\Kew\FrozenClock
+ *
+ * @internal
+ */
+final class FrozenClockTest extends TestCase
+{
+    public function test_returns_the_initially_provided_date_and_time(): void
+    {
+        $now = new \DateTimeImmutable();
+        $clock = new FrozenClock($now);
+
+        $this->assertEquals($now, $clock->now());
+        $this->assertNotSame($now, $clock->now());
+    }
+
+    public function test_can_be_set_to_return_a_different_date_and_time(): void
+    {
+        $clock = new FrozenClock(new \DateTimeImmutable());
+        $now = new \DateTimeImmutable();
+
+        $clock->setTo($now);
+
+        $this->assertEquals($now, $clock->now());
+        $this->assertNotSame($now, $clock->now());
+    }
+}
